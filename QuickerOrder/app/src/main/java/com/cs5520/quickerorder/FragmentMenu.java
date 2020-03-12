e022a7fab65dc6e7b702d1f86323d13ff07fdbe5
sinsanity <sun.yi5@husky.neu.edu>
2020-03-11 17:37:23 -0700

    make three fragments

--- QuickerOrder/app/src/main/java/com/cs5520/quickerorder/FragmentMenu.java
+++ QuickerOrder/app/src/main/java/com/cs5520/quickerorder/FragmentMenu.java
@@ -13,7 +13,8 @@ public class FragmentMenu extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
--- QuickerOrder/app/src/main/java/com/cs5520/quickerorder/ui/main/SectionsPagerAdapter.java
+++ QuickerOrder/app/src/main/java/com/cs5520/quickerorder/ui/main/SectionsPagerAdapter.java
@@ -17,7 +17,7 @@ import com.cs5520.quickerorder.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[]{R.string.full_menu, R.string.recommendation, R.string.call_service};
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
@@ -40,7 +40,8 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        // Show 2 total pages.
-        return 2;
+        // Show 3 total pages.
+        // dynamically
+        return TAB_TITLES.length;
     }
 }
